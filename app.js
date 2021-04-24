cd75e500c5c52fac2750c940085f658e7bcf5027
Erfan <erfan.fulldevs@gmail.com>
2021-04-24 10:32:29 +0430

    added mongoose to work with the db - testing . . .

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 const app = express();
 
+// connect to MongoDB
+const dbURI = 'mongodb+srv://erfanfulldev:test123@node.jcbmo.mongodb.net/Node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+        app.listen(3000, 'localhost', () => {
+            console.log('listening to  [ localhost:3000 ]');
+        })
+
+    }).catch(err => console.log(err.message));
+
 app.set('view engine', 'ejs');
 // app.set('views', 'myviews')
+
+
+
+
+app.use(express.static('public'))
+
+
+app.use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode < 200
+    }
+}));
+
+
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'New blog',
+        snippet: 'about my new blog',
+        body: 'more about my blog'
+    });
+
+    blog.save()
+        .then(result => {
+            res.send(result)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+
 
 app.get('/', (req, res) => {
     const blogs = [
@@ -27,6 +72,3 @@ app.use((req, res) => {
 })
 
 
-app.listen(3000, 'localhost', () => {
-    console.log('listening to  [ localhost:3000]');
-})
